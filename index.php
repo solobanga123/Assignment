626959b7414057f2e65302bbbb00af52f9387302
hvphi <50516445+hvphi@users.noreply.github.com>
2019-06-18 17:09:27 +0700

    Thay doi index

--- index.php
+++ index.php
@@ -1,120 +1,21 @@
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>My site</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="sheet/bootstrap.css">
-	<link rel="stylesheet" href="sheet/ionicons.min.css">
-	<link rel="stylesheet" href="sheet/mine.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,700" rel="stylesheet">
-	<script src="javascript/myscript.js"></script>
-</head>
- <body>
- <div class="menu">
-		<div class="container">
-			<div class="row">
-				<div class="col-12 text-center">
-					<h3 class="py-3">About <span>Me</span></h3>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="container" style="margin-top: 116px;">
-		<div class="row">
-			<div class="col-11 mx-auto profile">
-				<div class="py-3 px-2 px-lg-4 py-lg-5">
-
-					<div class="row">
-						<div class="avatarBox col-12 col-lg-5">
-							<img src='img/3.jpg' class="img-fluid">
-						</div>
-
-						<div class="infoBox col-12 col-lg-6 mt-2 mt-lg-0">
-							<p class="sayHi">Hello</p>
-
-							<div class="name">
-								<p><span>I'm</span> Ho Van Phi</p>
-								<p class="s">Verified
-									<span class="ion-ios-checkmark-outline"></span>
-								</p>
-								<p><span class="ion-android-globe"></span> Viethanit.edu.vn</p>
-							</div>
-							<hr>
-
-							<div class="detailInfo">
-								<div class="row my-2">
-									<div class="col-lg-4 col-12 boldd">Họ và tên</div>
-									<div class="col-lg-8 col-12 b"> Hồ Văn Phi </div>
-								</div>
-								<div class="row my-2">
-									<div class="col-lg-4 col-12 boldd">Đơn vị</div>
-									<div class="col-lg-8 col-12 b">Trường CĐ CNTT Hữu nghị Việt – Hàn- VietHanIT</div>
-								</div>
-								<div class="row my-2">
-									<div class="col-lg-4 col-12 boldd">Công việc</div>
-									<div class="col-lg-8 col-12 b">Giảng viên</div>
-								</div>
-                                                                <div class="row my-2">
-									<div class="col-lg-4 col-12 boldd">Trình độ</div>
-									<div class="col-lg-8 col-12 b">Tiến sĩ</div>
-								</div>
-                                                                <div class="row my-2">
-									<div class="col-lg-4 col-12 boldd">Chuyên ngành</div>
-									<div class="col-lg-8 col-12 b">Khoa học máy tính</div>
-								</div>
-								<div class="row">
-									<div class="col-lg-4 col-12 boldd">Email</div>
-									<div class="col-lg-8 col-12 b">phihv@viethanit.edu.vn</div>
-								</div>
-                                                                <div class="row">
-									<div class="col-lg-4 col-12 boldd">Số điện thoại</div>
-									<div class="col-lg-8 col-12 b">0905702411</div>
-								</div>
-                                                             <div class="row">
-									<div class="col-lg-4 col-12 boldd">More detail</div>
-									<div class="col-lg-8 col-12 b"><a href="MePro/Detail.html" target="_blank" >Click here</a> </div>
-								</div>
-
-							</div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-			<div class="col-11 mx-auto bg-danger contact text-center">
-				<div class="social col-12">
-					<!-- Facebook -->
-					<a href="https://facebook.com" target="_blank">
-						<span class="ion-social-facebook mr-4 mr-lg-5"></span>
-					</a>
-					<span class="ion-social-instagram-outline mr-4 mr-lg-5 ml-lg-3"></span>
-					<span class="ion-social-whatsapp-outline mr-4 mr-lg-5 ml-lg-3"></span>
-					<span class="ion-social-skype-outline mr-4 mr-lg-5 ml-lg-3"></span>
-					<span class="ion-social-twitter-outline mr-4 mr-lg-5 ml-lg-3"></span>
-					<span class="ion-social-linkedin-outline ml-lg-3"></span>
-				</div>
-			</div>
-		</div>
-	</div>
-
-     
-    
-     <a href="DatabaseIndex.php.html" target="_blank" >Work with Database</a> 
+    <a href="DatabaseIndex.php" target="_blank" >Work with database</a>
+     <br/> 
+    <a href="ConnectToDB.php" target="_blank" >Connect database</a>
+     <br/>
+     <a href="InsertData.php" target="_blank" >Insert data to the database</a>
+     <br/>
+     <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
+    <br/>
+    <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
+     <br/>
+     <a href="uploadfile.php" target="_blank" >upload file</a> 
+     <br/>
+     <a href="MyInfor.html" target="_blank" >My Information</a> 
      <br/>
      <a href="MePro/Detail.html" target="_blank" >Click here</a>
 
 </body>
-<footer>
-	<div class="container-fluid mt-2">
-		<div class="row">
-			<div class="col-11 text-right">
-				<p class="license mr-lg-4 mr-0">
-					<a href="https://Viethanit.edu.vn"></a>
-				</p>
-			</div>
-		</div>
-	</div>
-</footer>
+
      
 </html>
