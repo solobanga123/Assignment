72c615d478b3b4f36e02122a833a028da3f1ef0d
solobanga123 <54232596+solobanga123@users.noreply.github.com>
2019-08-22 11:48:31 +0700

    thay

---
+++ index_1.php
@@ -0,0 +1,17 @@
+<html>
+    <a href="DatabaseIndex.php" target="_blank" >Work with database</a>
+     <br/> 
+    <a href="ConnectToDB.php" target="_blank" >Connect database</a>
+     <br/>
+     <a href="InsertData.php" target="_blank" >Insert data to the database</a>
+     <br/>
+     <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
+    <br/>
+    <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
+     <br/>
+     
+
+</body>
+
+     
+</html>
